268e16d9477f0896c2de0a25b4da776f0e7e71aa
Alexey Abel <dev@abelonline>
2025-05-10 00:48:27 +0200

    Clean up shell.nix

--- shell.nix
+++ shell.nix
@@ -1,69 +1,47 @@
 {
-  pkgs ? import <nixpkgs> {
-    config.allowUnfree = true;
-  },
+  pkgs ? import <nixpkgs> { config.allowUnfree = true; },
 }:
 let
-  overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+  overrides = builtins.fromTOML (builtins.readFile ./rust-toolchain.toml);
   libPath =
     with pkgs;
     lib.makeLibraryPath [
       # load external libraries that you need in your rust project here
     ];
 in
-pkgs.mkShell rec {
+pkgs.mkShell {
   buildInputs = with pkgs; [
-    vscode.fhs
-    libxkbcommon # for slint server
-    clang
-    # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version (at the time of writing, 16)
-    llvmPackages_17.bintools
-    rustup
-    #libsForQt5.full # not sure if needed in addition to kdePackages.qtwayland
-    kdePackages.qtwayland # needed for icon to appear
-    #libGL
-    lldb
-    nil
-    nixfmt-rfc-style
-    pkg-config
-    openssl
-    evcxr
+    # IDEs
+    vscode.fhs # IDE
+    zed-editor # alternative IDE
 
-    # kdePackages.wayqt
-    # kdePackages.layer-shell-qt
-    kdePackages.qtsvg
-    # #libsForQt5.qt5.qtsvg
-    # kdePackages.qtbase
-    # kdePackages.qt5compat
-    # qt6Packages
-    # kdePackages.qt6ct
+    # Rust language
+    cargo
+    rust-analyzer # Language server for rust language
+    rustfmt
+    clippy # linter for Rust
+
+    # Nix language
+    nil # Language server for nix language (Presumably better than nixd)
+    nixfmt-rfc-style # Nix language formatter (executable is nixfmt)
+
+    # Debugger
+    lldb
+
+    # for VSCode Extensions
+    libxkbcommon # for slint server VSCode extension
+
+    # Qt 6 (used as Slint backend)
+    qt6.qtbase
   ];
+
   RUSTC_VERSION = overrides.toolchain.channel;
-  # https://github.com/rust-lang/rust-bindgen#environment-variables
-  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-  shellHook = ''
-    export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-    export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-    export RUST_LOG=debug
-  '';
+
   # Add precompiled library to rustc search path
   RUSTFLAGS = (
-    builtins.map (a: ''-L ${a}/lib'') [
+    builtins.map (a: "-L ${a}/lib") [
       # add libraries here (e.g. pkgs.libvmi)
     ]
   );
   LD_LIBRARY_PATH = libPath;
-  # Add glibc, clang, glib, and other headers to bindgen search path
-  BINDGEN_EXTRA_CLANG_ARGS =
-    # Includes normal include path
-    (builtins.map (a: ''-I"${a}/include"'') [
-      # add dev libraries here (e.g. pkgs.libvmi.dev)
-      pkgs.glibc.dev
-    ])
-    # Includes with special directory paths
-    ++ [
-      ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-      ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-      ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-    ];
 }
